c93de7a6455a5f4efef3ee4030f314374580945b
Markimoss <76181077+Markimoss@users.noreply.github.com>
2020-12-29 18:05:21 +0000

    Blue things move right

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+startFlight();
+
+function startFlight() {
+  let launchTime = 0;
+  let index = 0;
+  for (const interval of flyOrder()) {
+    const parameters = {
+      index,
+      launchTime
+    };
+
+    setTimeout(function () {
+      launchIt(parameters);
+    }, launchTime * 1000);
+
+    launchTime = launchTime + interval;
+    index = index + 1;
+  }
+
+  //launchIt(launchTime);
+}
+
+function launchIt(parameters) {
+  console.log("launchIt ", parameters);
+  var hammer = document.createElement("div");
+  hammer.style.position = "absolute";
+  hammer.style.top = "50%";
+  hammer.style.left = "0%";
+  hammer.style.width = "2em";
+  hammer.style.height = "2em";
+  hammer.style.background = "cornflowerblue";
+  hammer.style.animation = "1s forwards slidetoright";
+  hammer.textContent = parameters.index + ":" + parameters.launchTime;
+  document.body.appendChild(hammer);
+}
+
+function flyOrder() {
+  return [1, 3, 1, 2, 4, 1];
+}
